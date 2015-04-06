55364a6cd4044e5ba85f6c41e8c885a917022704
Patrick Farrell <mr.patfarrell@gmail.com>
2015-04-06 08:47:33 -0700

    number streak support created

--- bin/populate.rb
+++ bin/populate.rb
@@ -4,12 +4,24 @@ user.name='Pat F'
 user.email='mr.patfarrell@gmail.com'
 user.username='patf'
 user.save
+
 streak=Streak.new
 streak.title='test streak'
 defi=[]
 d=StreakEntryDefinition.new
 d.name='date'
 d.type=:date
+defi[0] = d
+streak.config = defi
+streak.user = user
+streak.save
+
+streak=Streak.new
+streak.title='number streak'
+defi=[]
+d=StreakEntryDefinition.new
+d.name='checkins'
+d.type=:number
 defi[0] = d
 streak.config = defi
 streak.user = user
